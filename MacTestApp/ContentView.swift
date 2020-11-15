cfaeb133d09694b56f6104d6c45f451e29f7bf57
Petteri Kamppuri <petteri.kamppuri@me.com>
2020-11-15 20:46:47 +0200

    Initial Commit

---
+++ MacTestApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacTestApp
+//
+//  Created by Petteri Kamppuri on 15.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
